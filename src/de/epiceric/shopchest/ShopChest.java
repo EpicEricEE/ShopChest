e5ed1918adb91617a0b45a00492431f3c2afb67a
Eric <biederteric.eb@gmail.com>
2016-06-09 20:34:10 +0200

    Ensure that all shop item get removed on disable

--- src/de/epiceric/shopchest/ShopChest.java
+++ src/de/epiceric/shopchest/ShopChest.java
@@ -20,6 +20,9 @@ import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.permission.Permission;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -267,6 +270,17 @@ public class ShopChest extends JavaPlugin {
     public void onDisable() {
         for (Shop shop : ShopUtils.getShops()) {
             ShopUtils.removeShop(shop, false);
+        }
+
+        for (World world : Bukkit.getWorlds()) {
+            for (Entity entity : world.getEntities()) {
+                if (entity instanceof Item) {
+                    Item item = (Item) entity;
+                    if (item.hasMetadata("shopItem")) {
+                        item.remove();
+                    }
+                }
+            }
         }
     }
 
