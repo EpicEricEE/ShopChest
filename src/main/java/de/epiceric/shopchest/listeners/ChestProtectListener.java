1de56fcf6ad59785c811cbcb23a10ced1e833cec
Eric <biederteric.eb@gmail.com>
2017-03-25 13:24:05 +0100

    Protect holograms from damage

--- src/main/java/de/epiceric/shopchest/listeners/ChestProtectListener.java
+++ src/main/java/de/epiceric/shopchest/listeners/ChestProtectListener.java
@@ -9,6 +9,7 @@ import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import de.epiceric.shopchest.ShopChest;
 import de.epiceric.shopchest.language.LanguageUtils;
 import de.epiceric.shopchest.language.LocalizedMessage;
+import de.epiceric.shopchest.nms.Hologram;
 import de.epiceric.shopchest.shop.Shop;
 import de.epiceric.shopchest.utils.Permissions;
 import de.epiceric.shopchest.utils.ShopUtils;
@@ -20,12 +21,14 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.Chest;
 import org.bukkit.block.DoubleChest;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.inventory.InventoryType;
@@ -189,6 +192,15 @@ public class ChestProtectListener implements Listener {
 
             }
 
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onHologramDamage(EntityDamageEvent e) {
+        if (e.getEntity() instanceof ArmorStand) {
+            if (Hologram.isPartOfHologram((ArmorStand) e.getEntity())) {
+                e.setCancelled(true);
+            }
         }
     }
 
