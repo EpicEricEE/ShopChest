e1162a2d6bb72d2ead05ff6cd6e03b6000f53fa1
Steven M <admin@sigmaroot.de>
2017-11-24 18:49:15 +0100

    Fix second book also for new generated books not beeing serialized and deserialized

--- src/main/java/de/epiceric/shopchest/utils/Utils.java
+++ src/main/java/de/epiceric/shopchest/utils/Utils.java
@@ -59,6 +59,7 @@ public class Utils {
             itemStack2.setItemMeta(bookMeta2);
 
             itemStack1 = decode(encode(itemStack1));
+            itemStack2 = decode(encode(itemStack2));
         }
 
         return itemStack1.isSimilar(itemStack2);
