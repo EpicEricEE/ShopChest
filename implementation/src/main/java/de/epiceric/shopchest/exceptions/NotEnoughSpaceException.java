d87afd44ab408ef8a2f93a6981aa15975211978f
Eric <biederteric.eb@gmail.com>
2020-03-19 15:05:18 +0100

    Delete old implementation

--- implementation/src/main/java/de/epiceric/shopchest/exceptions/NotEnoughSpaceException.java
+++
@@ -1,9 +0,0 @@
-package de.epiceric.shopchest.exceptions;
-
-public class NotEnoughSpaceException extends Exception {
-    private static final long serialVersionUID = 3718475607700458355L;
-
-    public NotEnoughSpaceException(String message) {
-        super(message);
-    }
-}
--- implementation/src/main/java/de/epiceric/shopchest/exceptions/WorldNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package de.epiceric.shopchest.exceptions;
-
-public class WorldNotFoundException extends Exception {
-    private static final long serialVersionUID = -555886332156936972L;
-
-    public WorldNotFoundException(String worldName) {
-        super("Could not find world with name \"" + worldName + "\"");
-    }
-}
