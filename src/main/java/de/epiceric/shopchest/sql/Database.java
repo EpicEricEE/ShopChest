EpicEricEE/ShopChest

commit 0b9aee33e0430b669f61f0419f7710a51b5965e0

More work on taxes

src/main/java/de/epiceric/shopchest/sql/Database.java
@@ -507,9 +507,9 @@ public abstract class Database {
      * @param type Whether the executor bought or sold
      * @param callback Callback that - if succeeded - returns {@code null}
      */
-    public void logEconomy(final Player executor, Shop shop, ShopProduct product, double price, Type type, final Callback<Void> callback) {
+    public void logEconomy(final Player executor, Shop shop, ShopProduct product, double price, double taxedPrice, Type type, final Callback<Void> callback) {
         final String query = "INSERT INTO " + tableLogs + " (shop_id,timestamp,time,player_name,player_uuid,product_name,product,amount,"
-                + "vendor_name,vendor_uuid,admin,world,x,y,z,price,type) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+                + "vendor_name,vendor_uuid,admin,world,x,y,z,price,taxed_price,type) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
         if (Config.enableEconomyLog) {
             new BukkitRunnable() {
@@ -536,7 +536,8 @@ public abstract class Database {
                         ps.setInt(14, shop.getLocation().getBlockY());
                         ps.setInt(15, shop.getLocation().getBlockZ());
                         ps.setDouble(16, price);
-                        ps.setString(17, type.toString());
+                        ps.setDouble(17, taxedPrice);
+                        ps.setString(18, type.toString());
                         ps.executeUpdate();
 
                         if (callback != null) {

src/main/java/de/epiceric/shopchest/event/ShopBuySellEvent.java
@@ -11,13 +11,15 @@ public class ShopBuySellEvent extends ShopEvent implements Cancellable {
     private Type type;
     private int newAmount;
     private double newPrice;
+    private double newTaxedPrice;
     private boolean cancelled;
 
-    public ShopBuySellEvent(Player player, Shop shop, Type type, int newAmount, double newPrice) {
+    public ShopBuySellEvent(Player player, Shop shop, Type type, int newAmount, double newPrice, double newTaxedPrice) {
         super(player, shop);
         this.type = type;
         this.newAmount = newAmount;
         this.newPrice = newPrice;
+        this.newTaxedPrice = newTaxedPrice;
     }
 
     /**
@@ -39,6 +41,13 @@ public class ShopBuySellEvent extends ShopEvent implements Cancellable {
      */
     public double getNewPrice() {
         return newPrice;
+    }
+
+    /**
+     * @return The taxed price which might be modified because of automatic item amount calculation
+     */
+    public double getNewTaxedPrice() {
+        return newTaxedPrice;
     }
 
     @Override

src/main/java/de/epiceric/shopchest/listeners/ShopInteractListener.java
@@ -722,12 +722,16 @@ public class ShopInteractListener implements Listener {
 
         String worldName = shop.getLocation().getWorld().getName();
 
-        double price = shop.getTaxedBuyPrice();
-        if (stack) price = (price / shop.getProduct().getAmount()) * amount;
+        double realPrice = shop.getBuyPrice();
+        double taxedPrice = shop.getTaxedBuyPrice();
+        if (stack) {
+            realPrice = (realPrice / shop.getProduct().getAmount()) * amount;
+            taxedPrice = (taxedPrice / shop.getProduct().getAmount()) * amount;
+        }
 
-        if (econ.getBalance(executor, worldName) >= price || Config.autoCalculateItemAmount) {
+        if (econ.getBalance(executor, worldName) >= taxedPrice || Config.autoCalculateItemAmount) {
 
-            int amountForMoney = (int) (amount / price * econ.getBalance(executor, worldName));
+            int amountForMoney = (int) (amount / taxedPrice * econ.getBalance(executor, worldName));
 
             if (amountForMoney == 0 && Config.autoCalculateItemAmount) {
                 executor.sendMessage(LanguageUtils.getMessage(Message.NOT_ENOUGH_MONEY));
@@ -770,29 +774,30 @@ public class ShopInteractListener implements Listener {
             if (newAmount > amount) newAmount = amount;
 
             ShopProduct newProduct = new ShopProduct(product, newAmount);
-            double newPrice = (price / amount) * newAmount;
+            double newRealPrice = (realPrice / amount) * newAmount;
+            double newTaxedPrice = (taxedPrice / amount) * newAmount;
 
             if (freeSpace >= newAmount) {
                 plugin.debug(executor.getName() + " has enough inventory space for " + freeSpace + " items (#" + shop.getID() + ")");
 
-                EconomyResponse r = econ.withdrawPlayer(executor, worldName, newPrice);
+                EconomyResponse r = econ.withdrawPlayer(executor, worldName, newTaxedPrice);
 
                 if (r.transactionSuccess()) {
-                    EconomyResponse r2 = (shop.getShopType() != ShopType.ADMIN) ? econ.depositPlayer(shop.getVendor(), worldName, newPrice) : null;
+                    EconomyResponse r2 = (shop.getShopType() != ShopType.ADMIN) ? econ.depositPlayer(shop.getVendor(), worldName, newRealPrice) : null;
 
                     if (r2 != null) {
                         if (r2.transactionSuccess()) {
-                            ShopBuySellEvent event = new ShopBuySellEvent(executor, shop, ShopBuySellEvent.Type.BUY, newAmount, newPrice);
+                            ShopBuySellEvent event = new ShopBuySellEvent(executor, shop, ShopBuySellEvent.Type.BUY, newAmount, newRealPrice, newTaxedPrice);
                             Bukkit.getPluginManager().callEvent(event);
 
                             if (event.isCancelled()) {
-                                econ.depositPlayer(executor, worldName, newPrice);
-                                econ.withdrawPlayer(shop.getVendor(), worldName, newPrice);
+                                econ.depositPlayer(executor, worldName, newTaxedPrice);
+                                econ.withdrawPlayer(shop.getVendor(), worldName, newRealPrice);
                                 plugin.debug("Buy event cancelled (#" + shop.getID() + ")");
                                 return;
                             }
 
-                            database.logEconomy(executor, shop, newProduct, newPrice, ShopBuySellEvent.Type.BUY, null);
+                            database.logEconomy(executor, shop, newProduct, newRealPrice, newTaxedPrice, ShopBuySellEvent.Type.BUY, null);
 
                             addToInventory(inventory, newProduct);
                             removeFromInventory(c.getInventory(), newProduct);
@@ -809,34 +814,34 @@ public class ShopInteractListener implements Listener {
 
                             String vendorName = (shop.getVendor().getName() == null ? shop.getVendor().getUniqueId().toString() : shop.getVendor().getName());
                             executor.sendMessage(LanguageUtils.getMessage(Message.BUY_SUCCESS, new Replacement(Placeholder.AMOUNT, String.valueOf(newAmount)),
-                                    new Replacement(Placeholder.ITEM_NAME, newProduct.getLocalizedName()), new Replacement(Placeholder.BUY_PRICE, String.valueOf(newPrice)),
+                                    new Replacement(Placeholder.ITEM_NAME, newProduct.getLocalizedName()), new Replacement(Placeholder.BUY_PRICE, String.valueOf(newTaxedPrice)),
                                     new Replacement(Placeholder.VENDOR, vendorName)));
 
                             plugin.debug(executor.getName() + " successfully bought (#" + shop.getID() + ")");
 
                             if (shop.getVendor().isOnline() && Config.enableVendorMessages) {
                                 shop.getVendor().getPlayer().sendMessage(LanguageUtils.getMessage(Message.SOMEONE_BOUGHT, new Replacement(Placeholder.AMOUNT, String.valueOf(newAmount)),
-                                        new Replacement(Placeholder.ITEM_NAME, newProduct.getLocalizedName()), new Replacement(Placeholder.BUY_PRICE, String.valueOf(newPrice)),
+                                        new Replacement(Placeholder.ITEM_NAME, newProduct.getLocalizedName()), new Replacement(Placeholder.BUY_PRICE, String.valueOf(newRealPrice)),
                                         new Replacement(Placeholder.PLAYER, executor.getName())));
                             }
 
                         } else {
                             plugin.debug("Economy transaction failed (r2): " + r2.errorMessage + " (#" + shop.getID() + ")");
                             executor.sendMessage(LanguageUtils.getMessage(Message.ERROR_OCCURRED, new Replacement(Placeholder.ERROR, r2.errorMessage)));
-                            econ.withdrawPlayer(shop.getVendor(), worldName, newPrice);
-                            econ.depositPlayer(executor, worldName, newPrice);
+                            econ.withdrawPlayer(shop.getVendor(), worldName, newRealPrice);
+                            econ.depositPlayer(executor, worldName, newTaxedPrice);
                         }
                     } else {
-                        ShopBuySellEvent event = new ShopBuySellEvent(executor, shop, ShopBuySellEvent.Type.BUY, newAmount, newPrice);
+                        ShopBuySellEvent event = new ShopBuySellEvent(executor, shop, ShopBuySellEvent.Type.BUY, newAmount, newRealPrice, newTaxedPrice);
                         Bukkit.getPluginManager().callEvent(event);
 
                         if (event.isCancelled()) {
-                            econ.depositPlayer(executor, worldName, newPrice);
+                            econ.depositPlayer(executor, worldName, newTaxedPrice);
                             plugin.debug("Buy event cancelled (#" + shop.getID() + ")");
                             return;
                         }
 
-                        database.logEconomy(executor, shop, newProduct, newPrice, ShopBuySellEvent.Type.BUY, null);
+                        database.logEconomy(executor, shop, newProduct, newRealPrice, newTaxedPrice, ShopBuySellEvent.Type.BUY, null);
 
                         addToInventory(inventory, newProduct);
                         executor.updateInventory();
@@ -851,14 +856,14 @@ public class ShopInteractListener implements Listener {
                         }.runTaskLater(plugin, 1L);
 
                         executor.sendMessage(LanguageUtils.getMessage(Message.BUY_SUCCESS_ADMIN, new Replacement(Placeholder.AMOUNT, String.valueOf(newAmount)),
-                                new Replacement(Placeholder.ITEM_NAME, newProduct.getLocalizedName()), new Replacement(Placeholder.BUY_PRICE, String.valueOf(newPrice))));
+                                new Replacement(Placeholder.ITEM_NAME, newProduct.getLocalizedName()), new Replacement(Placeholder.BUY_PRICE, String.valueOf(newTaxedPrice))));
 
                         plugin.debug(executor.getName() + " successfully bought (#" + shop.getID() + ")");
                     }
                 } else {
                     plugin.debug("Economy transaction failed (r): " + r.errorMessage + " (#" + shop.getID() + ")");
                     executor.sendMessage(LanguageUtils.getMessage(Message.ERROR_OCCURRED, new Replacement(Placeholder.ERROR, r.errorMessage)));
-                    econ.depositPlayer(executor, worldName, newPrice);
+                    econ.depositPlayer(executor, worldName, newTaxedPrice);
                 }
             } else {
                 executor.sendMessage(LanguageUtils.getMessage(Message.NOT_ENOUGH_INVENTORY_SPACE));
@@ -945,7 +950,7 @@ public class ShopInteractListener implements Listener {
 
                     if (r2 != null) {
                         if (r2.transactionSuccess()) {
-                            ShopBuySellEvent event = new ShopBuySellEvent(executor, shop, ShopBuySellEvent.Type.SELL, newAmount, newPrice);
+                            ShopBuySellEvent event = new ShopBuySellEvent(executor, shop, ShopBuySellEvent.Type.SELL, newAmount, newPrice, 0);
                             Bukkit.getPluginManager().callEvent(event);
 
                             if (event.isCancelled()) {
@@ -955,7 +960,7 @@ public class ShopInteractListener implements Listener {
                                 return;
                             }
 
-                            database.logEconomy(executor, shop, newProduct, newPrice, ShopBuySellEvent.Type.SELL, null);
+                            database.logEconomy(executor, shop, newProduct, newPrice, 0, ShopBuySellEvent.Type.SELL, null);
 
                             addToInventory(inventory, newProduct);
                             removeFromInventory(executor.getInventory(), newProduct);
@@ -991,7 +996,7 @@ public class ShopInteractListener implements Listener {
                         }
 
                     } else {
-                        ShopBuySellEvent event = new ShopBuySellEvent(executor, shop, ShopBuySellEvent.Type.SELL, newAmount, newPrice);
+                        ShopBuySellEvent event = new ShopBuySellEvent(executor, shop, ShopBuySellEvent.Type.SELL, newAmount, newPrice, 0);
                         Bukkit.getPluginManager().callEvent(event);
 
                         if (event.isCancelled()) {
@@ -1000,7 +1005,7 @@ public class ShopInteractListener implements Listener {
                             return;
                         }
 
-                        database.logEconomy(executor, shop, newProduct, newPrice, ShopBuySellEvent.Type.SELL, null);
+                        database.logEconomy(executor, shop, newProduct, newPrice, 0, ShopBuySellEvent.Type.SELL, null);
 
                         removeFromInventory(executor.getInventory(), newProduct);
                         executor.updateInventory();

src/main/java/de/epiceric/shopchest/shop/Shop.java
@@ -68,7 +68,7 @@ public class Shop {
         this.product = product;
         this.location = location;
         this.buyPrice = buyPrice;
-        this.taxedBuyPrice = calculateVat(buyPrice, Config.vat, Config.allowDecimalsInPrice);
+        this.taxedBuyPrice = buyPrice + calculateVat(buyPrice, Config.vat, Config.allowDecimalsInPrice);
         this.sellPrice = sellPrice;
         this.shopType = shopType;
     }
@@ -83,15 +83,15 @@ public class Shop {
      * @param price to make calculation on
      * @param vat in percentage
      * @param allowDecimal if round to int
-     * @return taxed price, returns price if price is equal to 0
+     * @return VAT, returns 0 if price is equal to 0
      */
     private double calculateVat(double price, double vat, boolean allowDecimal) {
         if (vat == 0) { // zero if disabled
-            return price;
+            return 0;
         }
 
-        price *= (1D + vat);
-        return allowDecimal ? price : (int) Math.max(price, 100 * vat);
+        double result = vat * price;
+        return allowDecimal ? result : (int) Math.max(result, 1);
     }
 
     /**

src/main/java/de/epiceric/shopchest/sql/MySQL.java
@@ -85,6 +85,7 @@ public class MySQL extends Database {
             + "y INTEGER NOT NULL,"
             + "z INTEGER NOT NULL,"
             + "price FLOAT NOT NULL,"
+            + "taxed_price FLOAT NOT NULL,"
             + "type TINYTEXT NOT NULL)";
     }
 

src/main/java/de/epiceric/shopchest/sql/SQLite.java
@@ -116,6 +116,7 @@ public class SQLite extends Database {
             + "y INTEGER NOT NULL,"
             + "z INTEGER NOT NULL,"
             + "price FLOAT NOT NULL,"
+            + "taxed_price FLOAT NOT NULL,"
             + "type TINYTEXT NOT NULL)";
     }
 

commit 7d5ce492b137ea64ebf4a06be6e4886cda5715d5

Fix database log query
0b9aee33e0430b669f61f0419f7710a51b5965e0 introduced a bug where economy
logs couldn't be submitted to the databse

src/main/java/de/epiceric/shopchest/sql/Database.java
@@ -509,7 +509,7 @@ public abstract class Database {
      */
     public void logEconomy(final Player executor, Shop shop, ShopProduct product, double price, double taxedPrice, Type type, final Callback<Void> callback) {
         final String query = "INSERT INTO " + tableLogs + " (shop_id,timestamp,time,player_name,player_uuid,product_name,product,amount,"
-                + "vendor_name,vendor_uuid,admin,world,x,y,z,price,taxed_price,type) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+                + "vendor_name,vendor_uuid,admin,world,x,y,z,price,taxed_price,type) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
         if (Config.enableEconomyLog) {
             new BukkitRunnable() {