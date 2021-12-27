51bee2dd20d9e1d0b62fd5528b359e37a17ffbfa
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2021-12-27 15:36:23 +0100

    Move plugin src directory to a sub module

--- src/main/java/de/epiceric/shopchest/exceptions/ChestNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package de.epiceric.shopchest.exceptions;
-
-public class ChestNotFoundException extends Exception {
-    private static final long serialVersionUID = -6446875473671870708L;
-
-    public ChestNotFoundException(String message) {
-        super(message);
-    }
-}
